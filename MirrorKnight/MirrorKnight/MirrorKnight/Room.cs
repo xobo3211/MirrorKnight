27eccd494b19e15d160efaa373a1aa09f3797182
kc <kenneth.chen.113@k12.friscoisd.org>
2019-04-02 12:12:27 -0500

    sad

---
+++ MirrorKnight/MirrorKnight/MirrorKnight/Room.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MirrorKnight
+{
+    class Room
+    {
+        int rType;
+        int rNo;
+        Random rand = new Random();
+        public Room(int rT, int rN)
+        {
+            rType = rT;
+            rNo = rN;
+        }
+        public Room(int rT)
+        {
+            rType = rT;
+            int c;
+            switch (rT)
+            {
+                case (0):
+                    c = 10; 
+                    break;
+                case (1):
+                    c = 10;
+                    break;
+                case (2):
+                    c = 10;
+                    break;
+                case (3):
+                    c = 10;
+                    break;
+                case (4):
+                    c = 10;
+                    break;
+            }
+            rNo = rand.Next(0, );
+        }
+        public int getRoomType()
+        {
+            return rType;
+        }
+        public int getRoomNo()
+        {
+            return rNo;
+        }
+        public void setRoomType(int r)
+        {
+            rType = r;
+        }
+        public void setRoomNo(int r)
+        {
+            rNo = r;
+        }
+        
+    }
+}
