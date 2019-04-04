3db71affa15da94400777ac962de5d1c612a6cb7
238948 <238948@S020RMD114-13>
2019-04-04 11:23:34 -0500

    Tile Class

--- MirrorKnight/MirrorKnight/MirrorKnight/Room.cs
+++ MirrorKnight/MirrorKnight/MirrorKnight/Room.cs
@@ -5,58 +5,48 @@ using System.Text;
 
 namespace MirrorKnight
 {
-    class Room
+    public class Room
     {
-        int rType;
-        int rNo;
-        Random rand = new Random();
-        public Room(int rT, int rN)
+        public enum Type
         {
-            rType = rT;
-            rNo = rN;
+            NORMAL,
+            TREASURE,
+            SHOP,
+            BOSS,
+            SECRET,
+            PUZZLE
         }
-        public Room(int rT)
+
+        Type roomType;
+
+        Tile[,] tiles;
+
+        public Room(Type t)
         {
-            rType = rT;
-            int c = 0; ;
-            switch (rT)
+            
+            switch (t)
             {
-                case (0): //Normal
-                    c = 10; 
+                case Type.NORMAL: //Normal
                     break;
-                case (1): //Boss
-                    c = 10;
+                case Type.TREASURE: //Treasure
                     break;
-                case (2): //Treasure
-                    c = 10;
+                case Type.SHOP: //Shop
                     break;
-                case (3): //Puzzle
-                    c = 10;
+                case Type.BOSS: //Boss
                     break;
-                case (4): //Shop
-                    c = 10;
+                case Type.SECRET: //Secret
                     break;
-                case (5): //Secret
-                    c = 10;
+                case Type.PUZZLE: //Puzzle
                     break;
             }
-            rNo = rand.Next(0, c);
         }
-        public int getRoomType()
+        public Type getRoomType()
         {
-            return rType;
+            return roomType;
         }
-        public int getRoomNo()
+        public void setRoomType(Type r)
         {
-            return rNo;
-        }
-        public void setRoomType(int r)
-        {
-            rType = r;
-        }
-        public void setRoomNo(int r)
-        {
-            rNo = r;
+            roomType = r;
         }
         
     }
---
+++ MirrorKnight/MirrorKnight/MirrorKnight/Tile.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using SureDroid;
+
+namespace MirrorKnight
+{
+    class Tile
+    {
+        public bool canMoveThrough;
+        public bool canShootThrough;
+        public bool damageHazard;
+
+        Texture2D texture;
+
+        public enum Type
+        {
+            NORMAL,
+            OBSTACLE,
+            PIT,
+            HAZARD
+        }
+
+        public Tile(Type t)
+        {
+            switch (t)
+            {
+                case Type.NORMAL:
+                    canMoveThrough = true;
+                    canShootThrough = true;
+                    damageHazard = false;
+                    break;
+
+                case Type.OBSTACLE:
+                    canMoveThrough = false;
+                    canShootThrough = false;
+                    damageHazard = false;
+                    break;
+
+                case Type.PIT:
+                    canMoveThrough = false;
+                    canShootThrough = true;
+                    damageHazard = false;
+                    break;
+
+                case Type.HAZARD:
+                    canMoveThrough = true;
+                    canShootThrough = true;
+                    damageHazard = true;
+                    break;
+            }
+
+        }
+
+        public void Draw(SpriteBatch b, int x, int y, int size)
+        {
+            b.Draw(texture, new Rectangle(x, y, size, size), Color.White);
+        }
+    }
+}
